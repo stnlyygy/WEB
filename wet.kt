4f3b930391e3ac5ae563b557e2ea032ff9aa90a3
stnlyygy <97420883+stnlyygy@users.noreply.github.com>
2022-02-08 10:18:06 +0700

    pertemuan ke-3

---
+++ wet.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+//    looping menggunakan while
+    var greeting = "hey tod"
+    var index = 1
+
+    while (index <= 5 )
+    {
+        println(greeting)
+        index++
+    }
+
+    do{
+        println(greeting)
+        index++
+    }
+        while (index <=5)
+}
---
+++ whenkotlin.kt
@@ -0,0 +1,11 @@
+fun main{
+    var index = 1
+
+    var greeting = "hey tod"
+    while (index <= 10) {
+        if (index == 8) {
+            println(greeting)
+        }
+        index++
+    }
+}
